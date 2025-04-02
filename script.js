f747314999c7850f8a800b955daf901e1eb222fd
Parthivjoshi29 <131014105+Parthivjoshi29@users.noreply.github.com>
2025-04-02 09:44:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// const HUGGINGFACE_API_KEY = "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";
-const HUGGINGFACE_API_KEY = "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";
+
+const HUGGINGFACE_API_KEY = "YOUR_API_KEY";
 
 let map, routeControl; // Updated global variables
 let destinations = []; // Array to store destination coordinates
